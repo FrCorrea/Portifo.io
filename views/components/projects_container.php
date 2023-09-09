e86b7eab5775deb6fe05b7aea03ce60e428766ce
Skyerv <amanda@manzoti.com>
2023-09-09 17:12:04 -0300

    feat: projects container

---
+++ views/components/projects_container.php
@@ -0,0 +1,22 @@
+<div class="projects-container">
+    <div class="projects-header">
+        <h3 class="projects-title"><i>Projects</i></h3>
+        <button type="button" class="btn btn-dark">Add new project</button>
+    </div>
+    <div class="websites-projects-container">
+        <h4 class="websites-projects-title">Websites</h4>
+        <div class="projects-carousel">
+            <?php include('views/components/websites_carousel.php') ?>
+        </div>
+
+        <h4 class="websites-projects-title">Apps</h4>
+        <div class="projects-carousel">
+            <?php include('views/components/websites_carousel.php') ?>
+        </div>
+
+        <h4 class="websites-projects-title">Designs</h4>
+        <div class="projects-carousel">
+            <?php include('views/components/websites_carousel.php') ?>
+        </div>
+    </div>
+</div>
