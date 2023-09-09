39cc7deff37e039a4fb3019055c9f5cab60fb6d5
Skyerv <amanda@manzoti.com>
2023-09-08 21:40:39 -0300

    add: create account page and first components

---
+++ views/components/footer.php
@@ -0,0 +1,5 @@
+<footer class="text-center text-lg-start text-white footer-default" style="background-color: #212529">
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+    Made with ♡ by <br /> <b>Amanda Rodrigues</b> & <b>Francisco Correa</b> </a>
+  </div>
+</footer>
--- views/header.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar sticky-top bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Sticky top</a>
-  </div>
-</nav>
