c84b7b6d7252a7d0da0b3a7b213ad038dcf2fa94
Skyerv <amanda@manzoti.com>
2023-09-10 22:20:24 -0300

    refactor: new navbar with tabs

--- views/components/header.php
+++ views/components/header.php
@@ -1,5 +1,21 @@
-<nav class="navbar bg-dark sticky-top bg-body-tertiary" data-bs-theme="dark">
+<nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark sticky-top" data-bs-theme="dark">
   <div class="container-fluid">
-  <a class="navbar-brand brand-name" href="#">Portfol.io</a>
+    <a class="navbar-brand brand-name" href="#">Portfol.io</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="views/pages/websites_page.php">Websites</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="apps_page.php">Apps</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="designs_page.php">Designs</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
