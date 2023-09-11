970fadc6f330662fcf2c428b5d195949c4e23aa9
Skyerv <amanda@manzoti.com>
2023-09-10 22:20:48 -0300

    feat: websites page

---
+++ views/pages/websites_page.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+if (empty($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
+    echo "entrou";
+    header('Location: login_page.php');
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Websites Page</title>
+
+    <link rel="stylesheet" type="text/css" href="views/assets/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+P+One&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:wght@100;300&family=Mochiy+Pop+P+One&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+
+    <?php require('views/components/header.php') ?>
+    <?php require('views/components/aside.php') ?>
+
+    <div class="projects-container">
+        <div class="projects-header">
+            <h3 class="projects-title"><i>Websites</i></h3>
+            <button type="button" class="btn btn-dark"> <a href="add_project_page.php"> Add Project </a> </button>
+        </div>
+        <div class="websites-projects-container">
+            <div class="projects-carousel">
+                <?php include('views/components/websites_carousel.php') ?>
+            </div>
+        </div>
+    </div>
+    <button type="button" class="btn btn-primary"> <a href="logout.php">Logout</a></button>
+</body>
+
+</html>
