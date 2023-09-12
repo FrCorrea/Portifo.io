19a8bd5cc3ff154b74eadf33096565d8c593963f
Skyerv <amanda@manzoti.com>
2023-09-12 18:35:14 -0300

    fix: fixed header link

--- views/components/header.php
+++ views/components/header.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="views/pages/websites_page.php">Websites</a>
+          <a class="nav-link" href="websites_page.php">Websites</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="apps_page.php">Apps</a>
