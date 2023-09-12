44fbd2481af0c30c7cc333217273cab9e8ac29ef
Skyerv <amanda@manzoti.com>
2023-09-12 18:36:37 -0300

    fix: minor adjustments on websites_page

--- views/pages/websites_page.php
+++ views/pages/websites_page.php
@@ -5,7 +5,6 @@ if (empty($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
     echo "entrou";
     header('Location: login_page.php');
 }
-
 ?>
 
 <!doctype html>
@@ -17,7 +16,7 @@ if (empty($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
 
     <title>Websites Page</title>
 
-    <link rel="stylesheet" type="text/css" href="views/assets/style.css">
+    <link rel="stylesheet" type="text/css" href="../assets/style.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -32,21 +31,22 @@ if (empty($_SESSION['logged_in']) || !$_SESSION['logged_in']) {
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 
-    <?php require('views/components/header.php') ?>
-    <?php require('views/components/aside.php') ?>
+    <?php include('../components/header.php') ?>
+    <?php include('../components/aside.php') ?>
 
     <div class="projects-container">
         <div class="projects-header">
             <h3 class="projects-title"><i>Websites</i></h3>
-            <button type="button" class="btn btn-dark"> <a href="add_project_page.php"> Add Project </a> </button>
+            <button type="button" class="btn btn-dark" onclick="location.href = 'add_project_page.php';">Add Project</button>
+
         </div>
         <div class="websites-projects-container">
             <div class="projects-carousel">
-                <?php include('views/components/websites_carousel.php') ?>
+                <?php include('../components/websites_carousel.php') ?>
             </div>
         </div>
     </div>
-    <button type="button" class="btn btn-primary"> <a href="logout.php">Logout</a></button>
+    <button type="button" class="btn btn-danger btn-logout" onclick="location.href = 'logout.php';">Logout</button>
 </body>
 
 </html>
