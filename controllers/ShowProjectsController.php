c6eee41c6044b19caba6ec00e8d3b80079fce623
Skyerv <amanda@manzoti.com>
2023-09-12 18:34:31 -0300

    fix: minor adjustments on `ShowProjectsController`

--- controllers/ShowProjectsController.php
+++ controllers/ShowProjectsController.php
@@ -7,9 +7,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $userId = $_SESSION['user_id'] ?? '';
     $type = $_POST['type'];
 
-    if (isset($_POST['type']) && isset($_POST['userId'])) {
+    if (isset($_POST['type']) && isset($userId)) {
         $type = $_POST['type'];
-        $userId = $_POST['userId'];
     
         // Encontre o usuário com base no userId
         $user = null;
@@ -30,8 +29,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
 
             // Retorna os projetos filtrados como JSON
-            header('Content-Type: application/json');
+            
+            //header('Content-Type: application/json');
             echo json_encode($filteredProjects);
+            $_SESSION['filtered_projects'] = json_encode($filteredProjects);
+            header('Location: ../views/pages/websites_page.php'); // Redireciona para a área protegida
         } else {
             // Retorna uma mensagem de erro se o usuário não for encontrado
             header('HTTP/1.1 404 Not Found');
