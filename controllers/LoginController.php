32be22732c869c09c53757ab64f536ef90fa88dd
SkyeRV <amanda@manzoti.com>
2023-10-24 21:01:55 -0300

    fix: changes made in class

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,57 +1,59 @@
 <?php
 session_start();
 
-include '../models/model.user.php';
-
-Class LoginController {
+class LoginController {
     public function login(){
-        global $users;
-        $raiz = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR;
+        echo "ola mundo";
 
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $email = $_POST['email'];
-            $password = $_POST['password'];
+        $caminho =  './views/pages/login_page.php';
+        header('Location: ' . $caminho);
+        // global $users;
+        // $raiz = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR;
+
+        // if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        //     $email = $_POST['email'];
+        //     $password = $_POST['password'];
         
-            if ($email === '' || $password === '') {
-                $caminho =  '/Portifo.io/views/pages/login_page.php?error=Preencha todos os campos';
-                header('Location: ' . $caminho);
-                exit;
-            }
+        //     if ($email === '' || $password === '') {
+        //         $caminho =  '/Portifo.io/views/pages/login_page.php?error=Preencha todos os campos';
+        //         header('Location: ' . $caminho);
+        //         exit;
+        //     }
         
-            if (strpos($email, '@') == false) {
-                $caminho = '/Portifo.io//views/pages/login_page.php?error=Email inválido';
-                header('Location: ' . $caminho);
-                $_COOKIE['contadorEntradas'] += 1;
-                exit;
-            }
+        //     if (strpos($email, '@') == false) {
+        //         $caminho = '/Portifo.io//views/pages/login_page.php?error=Email inválido';
+        //         header('Location: ' . $caminho);
+        //         $_COOKIE['contadorEntradas'] += 1;
+        //         exit;
+        //     }
         
-            $emailFound = false;
-            $userId = null;
-            $userName = null;
-            $userEmail = null;
+        //     $emailFound = false;
+        //     $userId = null;
+        //     $userName = null;
+        //     $userEmail = null;
         
-            foreach ($users as $user) {
-                if ($user['email'] === $email && $user['password'] === $password) {
-                    $emailFound = true;
-                    $userId = $user['id'];
-                    $userName = $user['name'];
-                    $userEmail = $user['email'];
-                    break; // Saia do loop assim que encontrar o email
-                }
-            }
+        //     foreach ($users as $user) {
+        //         if ($user['email'] === $email && $user['password'] === $password) {
+        //             $emailFound = true;
+        //             $userId = $user['id'];
+        //             $userName = $user['name'];
+        //             $userEmail = $user['email'];
+        //             break; // Saia do loop assim que encontrar o email
+        //         }
+        //     }
         
-            if ($emailFound) {
-                // Autenticação bem-sucedida, defina uma variável de sessão para marcar o usuário como logado
-                $_SESSION['user_id'] = $userId;
-                $_SESSION['user_name'] = $userName;
-                $_SESSION['user_email'] = $userEmail;
-                $_SESSION['logged_in'] = true;
-                $caminho =  '/Portifo.io/views/pages/home_page.php';
-                header('Location: ' . $caminho);
-            } else {
-                $caminho =  '/Portifo.io/?error=Email ou senha inválidos';
-                header('Location: ' . $caminho);
-            }
-        }
+        //     if ($emailFound) {
+        //         // Autenticação bem-sucedida, defina uma variável de sessão para marcar o usuário como logado
+        //         $_SESSION['user_id'] = $userId;
+        //         $_SESSION['user_name'] = $userName;
+        //         $_SESSION['user_email'] = $userEmail;
+        //         $_SESSION['logged_in'] = true;
+        //         $caminho =  '/Portifo.io/views/pages/home_page.php';
+        //         header('Location: ' . $caminho);
+        //     } else {
+        //         $caminho =  '/Portifo.io/?error=Email ou senha inválidos';
+        //         header('Location: ' . $caminho);
+        //     }
+        // }
     }
 }
--- index.php
+++ index.php
@@ -3,8 +3,8 @@ require("vendor/autoload.php");
 
 use Pecee\SimpleRouter\SimpleRouter as Router;
 
-Router::get('/Portifo.io', 'LoginController@login');
-Router::get('/Portifo.io/home-page', 'ProjectsController@showProjects');
-Router::get('/Portifo.io/add-project', 'ProjectsController@addProjects');
+Router::get('/', 'LoginController@login');
+Router::get('/home-page', 'ProjectsController@showProjects');
+Router::get('/add-project', 'ProjectsController@addProjects');
 
 Router::start();
